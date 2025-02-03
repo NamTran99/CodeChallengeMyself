1db5e99606a5848bba787049e86a831a8a64c217
NamTran <trandinhnam1199@gmail.com>
2025-02-04 01:21:58 +0700

    notification

--- myBase/src/main/java/com/example/mybase/extensions/ContextExt.kt
+++ myBase/src/main/java/com/example/mybase/extensions/ContextExt.kt
@@ -1,7 +1,10 @@
 package com.example.mybase.extensions
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.NotificationManager
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.content.pm.ShortcutManager
@@ -15,15 +18,15 @@ import android.util.DisplayMetrics
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import com.example.mybase.R
 import com.example.mybase.helper.isOnMainThread
+import com.example.mybase.helper.isOreoPlus
 import com.example.mybase.helper.isUpsideDownCakePlus
 
 val Context.telecomManager: TelecomManager get() = getSystemService(Context.TELECOM_SERVICE) as TelecomManager
 val Context.windowManager: WindowManager get() = getSystemService(Context.WINDOW_SERVICE) as WindowManager
 val Context.notificationManager: NotificationManager get() = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 val Context.shortcutManager: ShortcutManager get() = getSystemService(ShortcutManager::class.java) as ShortcutManager
-
-
 
 fun Context.getScreenSize(): Pair<Float, Float> {
     val displayMetrics = DisplayMetrics()
@@ -74,6 +77,27 @@ fun Context.openFullScreenIntentSettings(appId: String) {
         val uri = Uri.fromParts("package", appId, null)
         val intent = Intent(Settings.ACTION_MANAGE_APP_USE_FULL_SCREEN_INTENT)
         intent.data = uri
+        startActivity(intent)
+    }
+}
+
+@SuppressLint("StringFormatInvalid")
+fun Context.copyToClipboard(text: String) {
+    val clip = ClipData.newPlainText("saved text", text)
+    (getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager).setPrimaryClip(clip)
+    val toastText = getString(R.string.value_copied_to_clipboard_show)
+    toast(toastText)
+}
+
+fun Context.openNotificationSettings() {
+    if (isOreoPlus()) {
+        val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+        intent.putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+        startActivity(intent)
+    } else {
+        // For Android versions below Oreo, you can't directly open the app's notification settings.
+        // You can open the general notification settings instead.
+        val intent = Intent(Settings.ACTION_SETTINGS)
         startActivity(intent)
     }
 }
